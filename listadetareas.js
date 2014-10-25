bda7124af2bb700e23f608396d2b6de520ea21d4
gregory1308 <jgavilac@ucn.edu.co>
2014-10-25 14:23:34 -0500

    use of ng-submit

--- listadetareas.js
+++ listadetareas.js
@@ -1,4 +1,9 @@
 function ControladorTareas ($scope) {
    $scope.tareas = [{texto: 'Ser Super Heroico con AngularJS', hecho: true},
     {texto: 'Crear una app con angulajs', hecho: true}]
+
+    $scope.agregarTarea = function () {
+    	$scope.tareas.push({texto: $scope.textoNuevaTarea, hecho: true});
+    	$scope.textoNuevaTarea = '';
+    };
 }
